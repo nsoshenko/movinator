373af5a7ffa41b254936c205fa1d40e3cd5af082
nsoshenko <nikas.nyc@gmail.com>
2021-11-25 18:56:58 +0200

    added service worker basics for PWA

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-// import "./styles/movinator_main_screen.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
